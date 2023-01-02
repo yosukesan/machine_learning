6aa438327cf278a40b18c53e629b8f61e17511cb
Yosuke OTSUKI <y.otsuki30@gmail.com>
2023-01-02 23:37:15 +0900

    fixed: input data is splitted into test and train

--- 001.linear_regression/sklearn/main.py
+++ 001.linear_regression/sklearn/main.py
@@ -1,6 +1,7 @@
 
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
 import pandas as pd
@@ -15,20 +16,27 @@ if __name__=="__main__":
     x = ice_cream["アイス売り上げ"]
     y = ice_cream["来客数"]
 
-    # normalise
-    x = x / x.abs().max()
-    x = x.values.reshape(-1, 1)
-    y = y / y.abs().max()
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0) 
+    print('train size', x_train.shape, y_train.shape)
+    print('test size', x_test.shape, y_test.shape)
 
-    lr.fit(x, y)
+    # normalise
+    x_train = x_train / x_train.abs().max()
+    y_train = y_train / y_train.abs().max()
+
+    x = x.values.reshape(-1, 1)
+    x_train = x_train.values.reshape(-1, 1)
+    x_test = x_test.values.reshape(-1, 1)
+
+    lr.fit(x_train, y_train)
 
     # check result
-    print("result=", y, lr.predict(x))
+    print("result=", y_test, lr.predict(x_test))
 
     # check accuracy
-    print("accuracy = ", mean_squared_error(y, lr.predict(x)))
+    print("accuracy = ", mean_squared_error(y_test, lr.predict(x_test)))
 
     # plot
     plt.scatter(x, y)
-    plt.plot(x, lr.predict(x))
+    plt.plot(x_test, lr.predict(x_test))
     plt.show()
